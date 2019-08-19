9ded2f3666ffe65152b554c60402aa0c77833d7a
bessszilard <bessszilard@gmail.com>
2019-08-19 14:19:26 +0200

    L77. Bitmap Header Values

--- src/Drawing_Fractal_Images/src/Bitmap.cpp
+++ src/Drawing_Fractal_Images/src/Bitmap.cpp
@@ -6,11 +6,15 @@
  */
 
 #include "Bitmap.h"
+#include "BitmapFileHeader.h"
+#include "BitmapInfoHeader.h"
+
+using namespace caveofprogramming;
 
 namespace caveofprogramming {
 
 Bitmap::Bitmap(int width, int height) : m_width(width), m_height(height),
-										m_pPixels(new uint8_t[width * height * 3]) {
+										m_pPixels(new uint8_t[width * height * 3]{}) {
 
 }
 
@@ -19,7 +23,17 @@ void setPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue) {
 }
 
 bool Bitmap::write( string filename ) {
-	return true;
+	BitmapFileHeader fileHeader;
+	BitmapInfoHeader fileInfo;
+
+	fileHeader.fileSize = sizeof(BitmapFileHeader) + sizeof(BitmapInfoHeader) +
+						  m_width * m_height * 3;
+	fileHeader.dataOffset = sizeof(BitmapFileHeader) + sizeof(BitmapInfoHeader);
+
+	fileInfo.width  = m_height;
+	fileInfo.height = m_height;
+
+	return false;
 }
 
 
--- src/Drawing_Fractal_Images/src/BitmapInfoHeader.h
+++ src/Drawing_Fractal_Images/src/BitmapInfoHeader.h
@@ -18,7 +18,7 @@ namespace caveofprogramming {
 
 struct BitmapInfoHeader {
 	int32_t headerSize{40};
-	int32_t widht;
+	int32_t width;
 	int32_t height;
 	int16_t planes{1};
 	int16_t bitsPerPixel{24};
